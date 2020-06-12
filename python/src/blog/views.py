68ebb8797d878301582ced88219533e5646a40d2
foley803 <foley803@gmail.com>
2020-06-12 13:26:15 -0400

    Add routes, templates

--- python/src/blog/views.py
+++ python/src/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+fake_posts = [
+    {
+        'author': 'Rich',
+        'title': 'First Post',
+        'content': 'This is harder than I thought.',
+        'date_posted': '06/12/2020',
+    },
+    {
+        'author': 'Not Rich',
+        'title': 'Second Post',
+        'content': 'This is much harder than I thought.',
+        'date_posted': '06/12/2025',
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': fake_posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
-# Create your views here.
+
