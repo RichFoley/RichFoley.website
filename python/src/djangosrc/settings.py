f6f25a5fffa1a69e3e08367de142af0d7d315873
foley803 <foley803@gmail.com>
2020-06-14 21:08:15 -0400

    Add crispy forms

--- python/src/djangosrc/settings.py
+++ python/src/djangosrc/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
