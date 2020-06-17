04043952106eb132dd4bf0e14b3e19c7a71b305b
foley803 <foley803@gmail.com>
2020-06-17 08:49:18 -0400

    Add post view and links from home

--- python/src/blog/views.py
+++ python/src/blog/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from .models import Post
 
@@ -6,6 +7,18 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    context_object_name = 'post'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
