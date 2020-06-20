b40f1af921408bd66d4a6527a8af27bad19ca358
foley803 <foley803@gmail.com>
2020-06-19 21:27:21 -0400

    Password reset with email

--- python/src/djangosrc/settings.py
+++ python/src/djangosrc/settings.py
@@ -138,3 +138,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('GMAIL_ACCT')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_ACCT')
+SERVER_EMAIL = os.environ.get('GMAIL_ACCT')
